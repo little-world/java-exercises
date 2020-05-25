aafba0cca5e16e0919c6315525e2595a02fc1354
peter.van.rijn@little-world.nl <peter.van.rijn@little-world.nl>
2020-05-25 12:46:22 +0200

    added comment

--- src/io/littleworld/course/variables.java
+++ src/io/littleworld/course/variables.java
@@ -1,21 +1,27 @@
 
-// declare a variable
+// variable
 int x;
+/// declares a variable of type int with the name x
 
-// declare a default value
+// default value
 int x = 3;
+// declares a variable x with the value of 3
 
-// declare a constant
+// constant
 final int x = 3;
+/// declares x as a constant
 
-// declare other types
+
+// types
 double d = 3.14;
 boolean b = true;
 String s = "hello";
+/// other types; String is a class!
 
 // typecast
 double d = x;
 int x = (int) d;
+/// convert a double to an int
 
 // operators
 int a=4, b=3, c;
@@ -24,3 +30,4 @@ c = a - b;   // 1
 c = a * b;   // 12
 c = a / b;   // 1
 c = a % b;   // 1
+/// the basic math operators
