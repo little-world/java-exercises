7bd535d5dcfcf39c1e1b913dd5e9ce7d36d6bd5e
peter.van.rijn@little-world.nl <peter.van.rijn@little-world.nl>
2020-05-28 12:35:58 +0200

    added kotlin

--- src/io/littleworld/course/variables.java
+++ src/io/littleworld/course/variables.java
@@ -1,32 +1,41 @@
 
 /// variable
-int x;
-# declares a variable of type int with the name x with a very long comment
-
-/// default value
 int x = 3;
-# declares a variable x with the value of 3
+#// declares a variable of type int with the name x with a very long comment
+
+/// print
+System.out.println(x);
+// print x
+
+/// inferred
+var x = 3;
+#// the compiler infers the variable type; since java 12
+
+/// more variables
+int x = 3, y = 4;
+// declare more variables separated by a comma
 
 /// constant
 final int x = 3;
-# declares x as a constant
+#// declares x as a constant
 
 /// types
+long l = 100;
 double d = 3.14;
 boolean b = true;
 String s = "hello";
-# other types; String is a class!
+#// other primitive types; String is a class
 
 /// typecast
-double d = x;
-int x = (int) d;
-# convert a double to an int
+double d = x;     // implicit
+int x = (int) d;  // explicit
+#// convert a double to an int
 
 /// operators
-int a=4, b=3, c;
+int a = 4, b = 3, c;
 c = a + b;   // 7
 c = a - b;   // 1
 c = a * b;   // 12
 c = a / b;   // 1
 c = a % b;   // 1
-# the basic math operators
+#// basic math operators
---
+++ src/io/littleworld/course/variables.kt
@@ -0,0 +1,37 @@
+
+/// variable
+var x: Int = 3
+#// declares a variable of type Int with the name x
+
+/// inferred
+var x = 3
+#// declares a variable x with the value of 3
+
+/// more variables
+var x = 3; var y = 4
+// every variable is declared individual
+
+/// constant
+val x = 3
+#// declares x as a constant
+
+/// types
+var l: Long = 100
+var d: Double = 3.14
+var b: Boolean = true
+var s: String = "hello"
+#//  in Kotlin all numbers are classes
+
+/// typecast
+d = x.toDouble()
+x = d.toInt()
+#// every type cast with toX..()
+
+/// operators
+var a = 4; var b = 3; var c = 0;
+c = a + b   // 7
+c = a - b   // 1
+c = a * b   // 12
+c = a / b   // 1
+c = a % b   // 1
+#// basic math operators
